2f6528275e13823183aaa063ce3b17a934ac9812
totallyReal123 <mpolito@mcqstudent.org>
2020-12-14 15:41:09 -0500

    Started testing with structs

--- testing/rawread.py
+++ testing/rawread.py
@@ -1,33 +1,19 @@
 import obspy
-
-def chunks(lst:list, n:int):
-	new_array = []
-	for i in range(0, len(lst), n):
-		new_array.append(lst[i:i+n])
-	return new_array
-
-def list_to_string(lst:list):
-	temp = ''.join([str(elem) for elem in lst]) 
-	return temp
+import pyseedb
+import tempfile
 
 def main():
-	#create_temp_file()
 	file = open("examine.mseed", "rb")
 	data = file.read()
 	file.close()
 
-	bar = []
-	for byte in data:
-		bar.append(format(int(byte), '08b'))
+	foo = tempfile.TemporaryFile(mode="w+b")
+	foo.write(data)
+	foo.seek(0)
 
-	words_list = chunks(bar, 4)
-	frame_list = chunks(words_list, 16)
 
-	frame = frame_list[0 ]
-	for word in frame:
-		for byte in word:
-			print(chr(int(byte, 2)), "-", int(byte, 2), "-", byte)
+	st = obspy.read(foo)
+	print(st[0].stats)
 
 if __name__ == '__main__':
-	#main()
-	tr = obspy.read("examine.mseed")
+	main()
