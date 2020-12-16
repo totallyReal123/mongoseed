6ccb812634f4336a33c4f766125bb452a2634050
totallyReal23 <mpolito@mcqstudent.org>
2020-12-16 12:04:24 -0500

    Added vars for controlling pauses - Added variables for controlling the pauses betweeen querying channels. - Improved logging format (it looks better now) - Improved catching of errors

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def get_stream(channel: str):
 	fake_file.seek(0)
 
 	st = obspy.read(fake_file)
+	fake_file.close()
 	return st
 
 def get_data_timestamps(trace):
@@ -50,22 +51,25 @@ def get_data_timestamps(trace):
 
 	return time_correlation
 
-def serve_forever(channels: list, db: 'pymongo.database.Database'):
+def serve_forever(channels: list, db: 'pymongo.database.Database', batch_interval: float, single_interval: float):
 	while True:
 		for channel in channels:
-			tmpdict = {}
-
 			tr = get_stream(channel)[0]
 			timestamps = get_data_timestamps(tr)
 			data = list(map(int, tr.data))
-			collection = db[channel]
+			records_to_add = []
 
-			logging.info("Logging data for channel'" + channel +"'")
 			for i in range(len(data)):
-				tmpdict['timestamp'] = timestamps[i]
-				tmpdict['data'] = data[i]
-				collection.insert_one(tmpdict)
-		time.sleep(6)
+				temp_dict = {}
+				temp_dict['timestamp'] = timestamps[i]
+				temp_dict['data'] = data[i]
+				records_to_add.append(temp_dict)
+
+			db[channel].insert_many(records_to_add)
+			logging.info("Logged data for channel '" + channel +"'")
+
+			time.sleep(single_interval)
+		time.sleep(batch_interval)
 
 def main():
 	# Set output level of logger
@@ -74,16 +78,29 @@ def main():
 	# Load json data into variable
 	jdata = load_settings()
 
-	# Setup seedlink connection
-	channels = jdata['seedServer']['channels']
-	pyseed.init((str(jdata['seedServer']['address']), int(jdata['seedServer']['port'])))
+	# Setup variables from settings
+	try:
+		channels = jdata['seedServer']['channels']
 
-	# Setup mongodb connection
-	mongouri = "mongodb://" + jdata['mongoServer']['address'] + ":" + str(jdata['mongoServer']['port'])
+		seed_address = str(jdata['seedServer']['address'])
+		seed_port = int(jdata['seedServer']['port'])
+
+		mongouri = "mongodb://" + jdata['mongoServer']['address'] + ":" + str(jdata['mongoServer']['port'])
+		dbname = str(jdata['mongoServer']['databaseName'])
+
+		batch_interval = float(jdata['global']['batchInterval'])
+		single_interval = float(jdata['global']['singleInterval'])	
+	except KeyError as e:
+		key_name = ''.join(e.args)
+		logging.critical("Unable to find setting '" + key_name + "' in configuration file.")
+		sys.exit(1)
+
+
+	# Initialize pyseed
+	pyseed.init((seed_address, seed_port))
+
+	# Setup mongodb connection (and check and create missing collections automagically)
 	client = pymongo.MongoClient(mongouri)
-
-	# Check and setup database and collections (if any collection doesn't exist, create it)
-	dbname = str(jdata['mongoServer']['databaseName'])
 	db = client[dbname]
 
 	if not (dbname in client.list_database_names()):
@@ -98,14 +115,14 @@ def main():
 				logging.warning("MongoDB collection '" + channel + "' not found - creating automagically")
 				db.create_collection(channel)
 
+	# Start main loop
 	logging.info("Starting server main loop")
 	try:
-		serve_forever(channels, db)
+		serve_forever(channels, db, batch_interval, single_interval)
 	except KeyboardInterrupt:
-		pass
-	logging.info("Keyboard Interrupt detected - closing server")
+		logging.info("Keyboard Interrupt detected - closing server")
+		sys.exit(130)
 
 if __name__ == '__main__':
-        # Configure the logging module before starting
-        logging.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d-%m-%y %H:%M:%S')
         main()
